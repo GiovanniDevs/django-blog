b302293ca4d1dc0e102875fae492ccb58719099e
GiovanniDevs <damico.giovanni.90@gmail.com>
2026-01-28 14:03:00 +0000

    testing bugfix

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(4!6$)@m=k#r7x$d((%ld-+sw8i#s7zb(!3420+btooy)u49ri
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [, '.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
